7e07ce99800b5448ef26d74d02fdeb34ba627051
FrontendFlair <hamaadafzal404@gmail.com>
2024-09-15 15:21:29 -0700

    commit

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -8,13 +8,13 @@ import { Form, FormControl } from "@/components/ui/form";
 import CustomFormField from "../CustomFormField";
 import SubmitButton from "../SubmitButton";
 import { useState } from "react";
-import { userFormValidation } from "@/lib/validation";
+import { PatientFormValidation } from "@/lib/validation";
 import { useRouter } from "next/navigation";
-import { createUser } from "@/lib/actions/patient.actions";
+import { registerPatient } from "@/lib/actions/patient.actions";
 import toast from "react-hot-toast";
 import { FormFieldType } from "./PatientForm";
 import { FileUploader } from "../FileUploader";
-import { Doctors, GenderOptions, IdentificationTypes } from "@/constants";
+import { Doctors, GenderOptions, IdentificationTypes, PatientFormDefaultValues } from "@/constants";
 import { SelectItem } from "../ui/select";
 import Image from "next/image";
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
@@ -24,43 +24,72 @@ const RegisterForm = ({ user }: { user: User }) => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
-  const form = useForm<z.infer<typeof userFormValidation>>({
-    resolver: zodResolver(userFormValidation),
+  const form = useForm<z.infer<typeof PatientFormValidation>>({
+    resolver: zodResolver(PatientFormValidation),
     defaultValues: {
-      name: "",
-      email: "",
-      phone: "",
+      ...PatientFormDefaultValues,
+      name: user.name,
+      email: user.email,
+      phone: user.phone,
     },
   });
 
-  // 2. Define a submit handler.
-  async function onSubmit({
-    name,
-    email,
-    phone,
-  }: z.infer<typeof userFormValidation>) {
+  const onSubmit = async (values: z.infer<typeof PatientFormValidation>) => {
     setIsLoading(true);
 
+    // Store file info in form data as
+    let formData;
+    if (
+      values.identificationDocument &&
+      values.identificationDocument?.length > 0
+    ) {
+      const blobFile = new Blob([values.identificationDocument[0]], {
+        type: values.identificationDocument[0].type,
+      });
+
+      formData = new FormData();
+      formData.append("blobFile", blobFile);
+      formData.append("fileName", values.identificationDocument[0].name);
+    }
+
     try {
-      const userData = { name, email: email.trim().toLowerCase(), phone };
+      const patient = {
+        userId: user.$id,
+        name: values.name,
+        email: values.email,
+        phone: values.phone,
+        birthDate: new Date(values.birthDate),
+        gender: values.gender,
+        address: values.address,
+        occupation: values.occupation,
+        emergencyContactName: values.emergencyContactName,
+        emergencyContactNumber: values.emergencyContactNumber,
+        primaryPhysician: values.primaryPhysician,
+        insuranceProvider: values.insuranceProvider,
+        insurancePolicyNumber: values.insurancePolicyNumber,
+        allergies: values.allergies,
+        currentMedication: values.currentMedication,
+        familyMedicalHistory: values.familyMedicalHistory,
+        pastMedicalHistory: values.pastMedicalHistory,
+        identificationType: values.identificationType,
+        identificationNumber: values.identificationNumber,
+        identificationDocument: values.identificationDocument
+          ? formData
+          : undefined,
+        privacyConsent: values.privacyConsent,
+      };
 
-      const user = await createUser(userData);
+      const newPatient = await registerPatient(patient);
 
-      if (user && user.$id) {
-        // Appwrite uses $id for the user ID
-        router.push(`/patients/${user.$id}/register`);
-      } else {
-        console.error("User ID is undefined");
+      if (newPatient) {
+        router.push(`/patients/${user.$id}/new-appointment`);
       }
-      toast.success("User created successfully");
-      console.log("User data:", userData);
     } catch (error) {
       console.log(error);
-      toast.error(error.message);
-    } finally {
-      setIsLoading(false);
     }
-  }
+
+    setIsLoading(false);
+  };
 
   return (
     <div>
--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,6 +1,17 @@
-import { users } from "../appwrite.config";
-import { ID, Query } from "node-appwrite";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { databases, storage, users } from "../appwrite.config";
+import { ID, Query,InputFile } from "node-appwrite";
 import { parseStringify } from "../utils";
+
+// Directly assign the variables
+const PROJECT_ID = "66e371830030780800d0";
+const API_KEY = "standard_a86434ebb7d29947a108ffe82287f3f481e4f5962318a0c79c6091fa4dfe9670ba87a5ca71d93a7baf000cb09e8a91685294da49b0b6c3bbfae259481237d912baa6b20bc5ea4bd3816ace29f4719fd3b2b8e570d4c08ce5615c6965cbaabcb5e491a469f9d3de9d023a93d6017a389ed051e178175424585f06ff204e975368";
+const DATABASE_ID = "66e3725f003c347a7a34";
+const PATIENT_COLLECTION_ID = "66e37294000956bb2a5c";
+const DOCTOR_COLLECTION_ID = "66e372ca000946f0b5de";
+const APPOINTMENT_COLLECTION_ID = "66e372fd0025d0561cfc";
+const BUCKET_ID = "66e3734b00069588d0a7";
+const ENDPOINT = "https://cloud.appwrite.io/v1";
 
 // CREATE APPWRITE USER
 export const createUser = async (user: CreateUserParams) => {
@@ -12,7 +23,7 @@ export const createUser = async (user: CreateUserParams) => {
       ID.unique(),
       normalizedEmail,
       user.phone,
-      undefined, // Handle password if required by your Appwrite setup
+      undefined,
       user.name
     );
 
@@ -25,7 +36,7 @@ export const createUser = async (user: CreateUserParams) => {
 
       // Query the existing user by email
       const existingUser = await users.list([
-        Query.equal("email", [user.email.trim().toLowerCase()]), // Use normalized email
+        Query.equal("email", [user.email.trim().toLowerCase()]),
       ]);
 
       console.log("Existing User query result:", existingUser);
@@ -53,6 +64,63 @@ export const getUser = async (userId: string) => {
   } catch (error) {
     console.error(
       "An error occurred while retrieving the user details:",
+      error
+    );
+  }
+};
+
+// REGISTER PATIENT
+export const registerPatient = async ({
+  identificationDocument,
+  ...patient
+}: RegisterUserParams) => {
+  try {
+    // Upload file ->  // https://appwrite.io/docs/references/cloud/client-web/storage#createFile
+    let file;
+    if (identificationDocument) {
+      const inputFile =
+        identificationDocument &&
+        InputFile.fromBlob(
+          identificationDocument?.get("blobFile") as Blob,
+          identificationDocument?.get("fileName") as string
+        );
+
+      file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile);
+    }
+
+    // Create new patient document -> https://appwrite.io/docs/references/cloud/server-nodejs/databases#createDocument
+    const newPatient = await databases.createDocument(
+      DATABASE_ID!,
+      PATIENT_COLLECTION_ID!,
+      ID.unique(),
+      {
+        identificationDocumentId: file?.$id ? file.$id : null,
+        identificationDocumentUrl: file?.$id
+          ? `${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file.$id}/view??project=${PROJECT_ID}`
+          : null,
+        ...patient,
+      }
+    );
+
+    return parseStringify(newPatient);
+  } catch (error) {
+    console.error("An error occurred while creating a new patient:", error);
+  }
+};
+
+// GET PATIENT
+export const getPatient = async (userId: string) => {
+  try {
+    const patients = await databases.listDocuments(
+      DATABASE_ID!,
+      PATIENT_COLLECTION_ID!,
+      [Query.equal("userId", [userId])]
+    );
+
+    return parseStringify(patients.documents[0]);
+  } catch (error) {
+    console.error(
+      "An error occurred while retrieving the patient details:",
       error
     );
   }
